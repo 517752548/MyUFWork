b33b144844cf169305d420f43738b4b5cdd8f79d
lv <517752548@qq.com>
2019-12-08 17:11:39 +0800

    add

--- MyUFGame/Assets/FastCore/Editor/Bundle/BundleMenuTool.cs
+++ MyUFGame/Assets/FastCore/Editor/Bundle/BundleMenuTool.cs
@@ -8,6 +8,9 @@ public class BundleMenuTool
     [MenuItem ("Fast/Bundle/ReInit")]
     static void CopyAssetPath ()
     {
-       
+        //生成资源根路径
+        DirectoryTool.CreatIfNotExists(Application.dataPath + FrameWorkConst.FastBundleResFolder);
+        DirectoryTool.CreatIfNotExists(Application.dataPath + FrameWorkConst.FastBundleResConfigFolder);
+        AssetDatabase.Refresh();
     }  
 }
--- MyUFGame/Assets/FastCore/FrameWorkConst.cs
+++ MyUFGame/Assets/FastCore/FrameWorkConst.cs
@@ -15,22 +15,16 @@ public class FrameWorkConst
     ///bundle 清单文件存放位置
     /// </summary>
     public const string assetsManifesttxt = "Assets/AssetsPackage/UFConfig/Manifest.txt";
-    public const string assetsRuletxt = "Assets/AssetsPackage/UFConfig/Rules.txt";
-    public const string OnLineAssetsRuletxt = "Assets/SkinResource/Rules.txt";
     public const string assetsManifesttxtFolder = "Assets/AssetsPackage/UFConfig";
     
     
     public const string addressAssetsManifesttxt = "Assets/AssetsPackage/AnsycLoad/UFConfig/addressAssetsConfig.txt";
     public const string addreesablesAssetsRuletxt = "Assets/AssetsPackage/AnsycLoad/UFConfig/AddressablesRules.txt";
-
-    /// <summary>
-    /// 只要路径下包含这个名称，这个bundle就会被单独打出来
-    /// </summary>
-    public const string singleBundle = "singlebundle";
-
-    public const string sharedBundle = "shared/";
     
-    public const string AssetBundlesOutputPath = "AssetBundles";
 
-    public const string SpriteAtlas = "spriteatlas";
+
+
+
+    public const string FastBundleResFolder = "/AssetsPackage/";
+    public const string FastBundleResConfigFolder = "/AssetsPackage/UFConfig/";
 }
