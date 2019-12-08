da1afd81e72887c77149419d36222992ee57995e
lv <517752548@qq.com>
2019-12-08 09:02:54 +0800

    add

---
+++ MyUFGame/Assets/FastCore/Editor/Bundle/BuildAddressablesData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildAddressablesData
+{
+    public string GroupName;
+    public string[] Lable;
+    public string ResType;
+    public string packageType;
+    public bool canUpdate;
+    public Dictionary<string,string> entitys = new Dictionary<string, string>();
+}
---
+++ MyUFGame/Assets/FastCore/Script/Res/File/ResLocation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResLocation
+{
+
+}
+public enum ResLoadLocation
+{
+    Resource,
+    Develop,
+    Streaming,
+    Persistent,
+    Catch,
+}
