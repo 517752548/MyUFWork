317037eeaaaa65a82c5b4e1642ceed48e6251ccb
lv <517752548@qq.com>
2019-12-29 10:14:00 +0800

    add

--- MyUFGame/Assets/FastCore/DefineConst.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class DefineConst
-{
-    public const string Addressable = "Define_Addressable";
-    public const string Notification = "Define_Notification";
-}
