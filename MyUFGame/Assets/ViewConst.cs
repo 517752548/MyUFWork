f0e75e9b16e20bc6e8c27a4be21db827c8e5428b
lv <517752548@qq.com>
2020-09-19 22:16:28 +0800

    add

--- MyUFGame/Assets/ViewConst.cs
+++ MyUFGame/Assets/ViewConst.cs
@@ -1,4 +1,6 @@
 public class ViewConst{
 //Group：txt ;Label:txt;
      public const string txt_AddressablesRules = "AddressablesRules.txt";
+//Group：txt ;Label:txt;
+     public const string txt_addressAssetsConfig = "addressAssetsConfig.txt";
 }
