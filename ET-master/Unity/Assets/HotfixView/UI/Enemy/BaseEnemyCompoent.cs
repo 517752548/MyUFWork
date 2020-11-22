ba7fd3c81c83e76f811beed459485b0739204768
lv <517752548@qq.com>
2020-11-22 20:33:50 +0800

    add

---
+++ ET-master/Unity/Assets/HotfixView/UI/Enemy/BaseEnemyCompoent.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using ET;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ET
+{
+    public class JLoginCompoent: UIBaseComponent
+    {
+        public string userid;
+        public string parentName;
+        
+        public override void OnOpen()
+        {
+            Log.Info("login open");
+            GameObject.Get<GameObject>("LoginBtn").GetComponent<Button>().onClick.AddListener(ClickLogin);
+            userid = SystemInfo.deviceUniqueIdentifier;
+            base.OnOpen();
+            GetTip().Coroutine();
+            parentName = this.parent.Id.ToString();
+        }
+        
+
+        public async  ETVoid GetTip()
+        {
+        }
+
+        public void ClickLogin()
+        {
+            Log.Info("click");
+            ET.Game.Scene.GetComponent<MapManagerComponent>().LoadMapAsync(1).Coroutine();
+            this.GetParent<UIBase>().CloseSelf();
+        }
+    }
+}
